f2ce292b870e2a981b6606be2a139f89ce67fc27
menawi <omar.menawi@gmail.com>
2022-12-24 01:41:16 -0500

    strast

--- javascriptWithTraversy/Vid_1/📌Vid1_Code/javascript/index3.js
+++ javascriptWithTraversy/Vid_1/📌Vid1_Code/javascript/index3.js
@@ -1,2 +1,60 @@
-// console.log('test print') ✅ 12/18/22
+// console.log('test print') 
 
+
+const todo = [
+  {
+    id: 1,
+    text: 'take out trash',
+    isCompleted: true
+  },
+
+  {
+    id: 2,
+    text: 'meeting with boss',
+    isCompleted: true
+  },
+
+  {
+    id: 3,
+    text: 'dentist appt',
+    isCompleted: false
+  }
+
+]
+
+// create variable that is JSON format
+    // const todoJSON = JSON.stringify(todo);
+    // console.log(todoJSON)
+
+    // 📌JSON important because it is how APIs send and receive data
+
+    // 📍 for loops
+
+for (let i = 0; i <10; i++){
+  console.log(i)
+}
+
+for(let i = 0; i<=10; i++)
+{
+  console.log(`For Loop Number: ${i}`);
+}
+
+// 📍 while loops
+// variable set outside the loop
+
+let i = 0
+while(i<10){
+  console.log(`while loop number: ${i}`);
+  i++ //if you NOT include this, while loop will run forever and your laptop will freak out and explode and burn (partially true)
+}
+
+
+// 📍loop through arrays
+
+for(let i = 0; i <todo.length; i++){
+  console.log(todo[i].text);
+}
+
+for(let todo of todo){
+  console.log(todo.text);
+}
